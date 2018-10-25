2f0922b933272570348a12a1e9c558bb496bd777
mordes <csongorhegedus2@indamail.hu>
2018-10-25 19:14:53 +0200

    Test Cs

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -50,7 +50,7 @@ document.addEventListener('init', function(event) {
 
     var page = event.target;
     if (page.matches('#helloworld-page')) {
-        page.querySelector('ons-toolbar .center').innerHTML = 'My app';
+        page.querySelector('ons-toolbar .center').innerHTML = 'Billy';
         page.querySelector('ons-button').onclick = function() {
             ons.notification.alert('Hello world!');
         };
